69be55a5d203f52869cbaaff42a97d1dbe87dc75
stf98 <sebastiantofra98@javerianacali.edu.co>
2020-06-07 22:51:49 -0500

    Finished API rest communication

---
+++ frontMoviles/Configuration/EndPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace frontMoviles.Configuration
+{
+    public class EndPoint
+    {
+        //Here we put the enpoints created on NodeJS
+        //public static readonly string URL_SERVIDOR = "http://3.23.254.163:8000";
+        //public static readonly string CONSULTAR_ALL_CATEGORIAS = "/all";
+        //public static readonly string CONSULTAR_CATEGORIA = "/get";
+        //public static readonly string CREAR_CATEGORIA = "/create";
+        //public static readonly string EDITAR_CATEGORIA = "/update";
+        //public static readonly string ELIMINAR_CATEGORIA = "/delete";
+    }
+}
--- frontMoviles/Services/ApiRest/ChooseRequest.cs
+++ frontMoviles/Services/ApiRest/ChooseRequest.cs
@@ -2,12 +2,46 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using frontMoviles.Configuration;
 using frontMoviles.Models.AuxiliarModels;
 
 namespace frontMoviles.Services.ApiRest
 {
-    public class ChooseRequest
+    public class ChooseRequest<T>
     {
+        #region Properties
+        public Request<T> EstrategiaEnvio { get; set; }
+        public ConfigurationRest ConfigurationRest { get; set; }
+        #endregion Properties
 
+        #region Initialize
+        public ChooseRequest()
+        {
+            ConfigurationRest = new ConfigurationRest();
+        }
+        #endregion Initialize
+
+        #region Métodos
+        public void ElegirEstrategia(string verb, string url)
+        {
+            var diccionario = ConfigurationRest.VerbsConfiguration;
+            string nombreClase;
+            if (diccionario.TryGetValue(verb.ToUpper(), out nombreClase))
+            {
+                Type tipoClase = Type.GetType(nombreClase);
+                Type[] typeArgs = { typeof(T) };
+                var genericClass = tipoClase.MakeGenericType(typeArgs);
+                EstrategiaEnvio = (Request<T>)Activator.CreateInstance(genericClass, url, verb.ToUpper());
+            }
+        }
+
+        public async Task<APIResponse> EjecutarEstrategia(T objecto, ParametersRequest parametersRequest = null)
+        {
+            parametersRequest = parametersRequest ?? new ParametersRequest();
+            await EstrategiaEnvio.ContruirURL(parametersRequest);
+            var response = await EstrategiaEnvio.SendRequest(objecto);
+            return response;
+        }
+        #endregion Métodos
     }
 }
