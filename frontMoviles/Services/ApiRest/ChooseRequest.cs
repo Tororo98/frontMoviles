a3f8fe121b62792af7a6539b58a4bd9fcc4a34bf
stf98 <sebastiantofra98@javerianacali.edu.co>
2020-06-07 19:47:58 -0500

    API rest modified

---
+++ frontMoviles/Services/ApiRest/ChooseRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using frontMoviles.Models.AuxiliarModels;
+
+namespace frontMoviles.Services.ApiRest
+{
+    public class ChooseRequest
+    {
+
+    }
+}
